c6f62f7c15a24cced4cbd60d6c22a8debe6d1313
Cheeze06 <74716470+Cheeze06@users.noreply.github.com>
2024-05-10 20:28:28 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="5대범죄발생률",
+    page_icon="🚨",
+)
+
+st.title('데이터분석')
+
+st.sidebar.success("위 내용은 몇년간 데이터 자료 분석을 통해 지원합니다.")
+
+st.header('ㅁㄴㅇㄻㄴㅇㄹ')
